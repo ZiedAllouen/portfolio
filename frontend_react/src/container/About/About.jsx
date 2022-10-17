05123659dc2999663330567d75d8c9e8f29240c0
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-10-17 20:33:55 +0100

    skils vf

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,7 +3,7 @@ import './About.scss'
 import { urlFor,client } from '../../client';
 import {motion} from 'framer-motion';
 import {images} from '../../constants';
-import { AppWrap } from '../../wrapper';
+import { AppWrap,MotionWrap } from '../../wrapper';
 
 const About = () => {
   const [abouts,setAbouts]=useState([]);
@@ -40,4 +40,8 @@ const About = () => {
   )
 }
 
-export default AppWrap(About,'about')
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
